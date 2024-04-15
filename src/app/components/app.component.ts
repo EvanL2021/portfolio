a82366c1c3134cb381dae6aa7d22f5f5caee52de
Evan <arkovlechauve.dev@gmail.com>
2024-04-15 23:42:28 +0200

    Added contact part

--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -4,11 +4,13 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { PresentationComponent } from './presentation/presentation.component';
 import { CompetencesComponent } from './competences/competences.component';
+import { ContactComponent } from './contact/contact.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, PresentationComponent, CompetencesComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, PresentationComponent, CompetencesComponent, ContactComponent, HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/components/contact/contact.component.ts
@@ -0,0 +1,42 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+
+@Component({
+  selector: 'app-contact',
+  standalone: true,
+  imports: [ReactiveFormsModule, MatButtonModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+  contactForm = new FormGroup({
+    email: new FormControl('', [Validators.required, Validators.email]),
+    name: new FormControl('', [Validators.required, Validators.minLength(5)]),
+    body: new FormControl('', [Validators.required, Validators.minLength(25)]),
+  });
+
+  constructor(private httpClient: HttpClient) {};
+
+  sendMail() {
+    const {email, name, body} = this.contactForm.value;
+
+    const httpOptions = {
+      headers: new HttpHeaders({
+        Accept: "application/json",
+        "Content-Type": "application/x-www-form-urlencoded"
+      })
+    };
+
+    this.httpClient.post<any>("https://formspree.io/f/xjvnpnva", `name=${name}&email=${email}&message=${body}`, httpOptions).subscribe({
+      next: data => {
+        window.confirm('E-mail envoyé !')
+      },
+      error: error => {
+          console.log('error!', error.message);
+      }
+  })    
+  }
+}
